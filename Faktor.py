8a190afa774319accf26eb091730a3e2d9ebc43a
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2020-04-03 09:25:15 +0300

    Delete Faktor.py

--- Faktor.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def Faktor(totalPublished,impactFactor):
-    impactFactor -= 0.99
-    result = math.ceil(totalPublished*impactFactor)
-    return result
-    
-x,y = map(int, input().split())
-print(Faktor(x,y))
